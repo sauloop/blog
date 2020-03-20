21c04743d173b8d259c4b6d98a2da8c0aa1a1382
Pablo Giraldo <pablogirald@yahoo.es>
2020-03-20 21:13:36 +0100

    Change img location

--- src/info/pablogiraldo/blog/config/DbConnection.java
+++ src/info/pablogiraldo/blog/config/DbConnection.java
@@ -12,11 +12,17 @@ public class DbConnection {
 //	private static String user = "root";
 //	private static String pass = "";
 
-	// Prod
+	// Prod pruebasblog
 
 	private static String url = "jdbc:mysql://gi6kn64hu98hy0b6.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/x4y287x0mhb2xxtd";
 	private static String user = "s1o3v3rkbdhm1bc7";
 	private static String pass = "jb6i5e6gv772zidi";
+
+	// Prod blogwar
+	
+//	private static String url = "jdbc:mysql://sabaik6fx8he7pua.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/ktqz1wuhfsknweaa";
+//	private static String user = "gi339d995q304vv7";
+//	private static String pass = "cu4vwab283ehef6z";
 
 	public static Connection getConnection() {
 
