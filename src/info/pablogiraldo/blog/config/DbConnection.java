efe278f7ec56a3c32599bf28094b70257a2f264b
Pablo Giraldo <pablogirald@yahoo.es>
2020-03-20 19:21:32 +0100

    Change heroku config

--- src/info/pablogiraldo/blog/config/DbConnection.java
+++ src/info/pablogiraldo/blog/config/DbConnection.java
@@ -14,9 +14,9 @@ public class DbConnection {
 
 	// Prod
 
-	private static String url = "jdbc:mysql://gi6kn64hu98hy0b6.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/v8ma475jhqbkm8bg";
-	private static String user = "pvwbl8zr6yvbtynf";
-	private static String pass = "pippn1havhufqy9d";
+	private static String url = "jdbc:mysql://gi6kn64hu98hy0b6.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/x4y287x0mhb2xxtd";
+	private static String user = "s1o3v3rkbdhm1bc7";
+	private static String pass = "jb6i5e6gv772zidi";
 
 	public static Connection getConnection() {
 
