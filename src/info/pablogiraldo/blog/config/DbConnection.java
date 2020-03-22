3efbfbdfc7a3c7a8d0dc41e1260e2a7da7c5d83a
Pablo Giraldo <pablogirald@yahoo.es>
2020-03-22 15:51:44 +0100

    Change config to prod

--- src/info/pablogiraldo/blog/config/DbConnection.java
+++ src/info/pablogiraldo/blog/config/DbConnection.java
@@ -6,18 +6,19 @@ import java.sql.SQLException;
 
 public class DbConnection {
 
+//	private static String url = "jdbc:mysql://localhost:3306/db_blog_b?useUnicode=yes&characterEncoding=UTF-8";
+
 	// Dev
 
-	private static String url = "jdbc:mysql://localhost:3306/db_blog";
-//	private static String url = "jdbc:mysql://localhost:3306/db_blog_b?useUnicode=yes&characterEncoding=UTF-8";
-	private static String user = "root";
-	private static String pass = "";
+//	private static String url = "jdbc:mysql://localhost:3306/db_blog";
+//	private static String user = "root";
+//	private static String pass = "";
 
 	// Prod pruebasblog
 
-//	private static String url = "jdbc:mysql://gi6kn64hu98hy0b6.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/x4y287x0mhb2xxtd";
-//	private static String user = "s1o3v3rkbdhm1bc7";
-//	private static String pass = "jb6i5e6gv772zidi";
+	private static String url = "jdbc:mysql://gi6kn64hu98hy0b6.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/x4y287x0mhb2xxtd";
+	private static String user = "s1o3v3rkbdhm1bc7";
+	private static String pass = "jb6i5e6gv772zidi";
 
 	// Prod blogwar
 
